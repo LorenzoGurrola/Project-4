7d6e7a8a45a5bffdd87d389797a1242f58f9474c
LorenzoGurrola <lorenzogurrola521@gmail.com>
2024-08-28 15:27:27 -0500

    completed project

--- src/train_model.py
+++ src/train_model.py
@@ -20,11 +20,19 @@ class NeuralNetwork(nn.Module):
         super(NeuralNetwork, self).__init__()
         self.l1 = nn.Linear(n, h)
         self.l2 = nn.Linear(h, 1)
+        self.loss_function = nn.BCELoss()
     
     def forward(self, X):
         A1 = F.relu(self.l1(X))
         A2 = torch.sigmoid(self.l2(A1))
         return A2
+    
+    def calculate_loss(self, yhat, y):
+        loss = self.loss_function(yhat, y)
+        return loss
+    
+    def backward(self, loss):
+        loss.backward()
 
 def load_data():
     data = pd.read_csv('../framingham.csv')
@@ -60,7 +68,35 @@ def forward(X, params):
     Z1 = X @ W1 + b1
     A1 = relu(Z1)
 
+    inter_vals = {'Z1':Z1, 'A1':A1}
+
     z2 = A1 @ w2 + b2
     a2 = sigmoid(z2)
 
-    return a2
+    return a2, inter_vals
+
+def calculate_cost(yhat, y):
+    m = y.shape[0]
+    losses = y * np.log(yhat) + (1 - y) * np.log(1 - yhat)
+    cost = -np.sum(losses, axis=0, keepdims=True)/m
+    return cost
+
+def backward(y, yhat, inter_vals, X, params):
+    m = y.shape[0]
+    A1 = inter_vals['A1']
+    Z1 = inter_vals['Z1']
+    w2 = params['w2']
+    dc_dyhat = (-1/m) * ((y/yhat) - ((1 - y)/(1 - yhat)))
+    dyhat_dz2 = yhat * (1 - yhat)
+    dc_dz2 = dc_dyhat * dyhat_dz2
+    dc_db2 = np.sum(dc_dz2, axis=0, keepdims=True)
+    dc_dw2 = np.matmul(A1.T, dc_dz2)
+
+    dc_dA1 = np.matmul(dc_dz2, w2.T)
+    dA1_dZ1 = np.where(Z1 >= 0, 1, 0)
+    dc_dZ1 = dc_dA1 * dA1_dZ1
+    dc_db1 = np.sum(dc_dZ1, axis=0, keepdims=True)
+    dc_dW1 = np.matmul(X.T, dc_dZ1)
+
+    grads = {'dW1':dc_dW1, 'db1':dc_db1, 'dw2':dc_dw2, 'db2':dc_db2}
+    return grads
